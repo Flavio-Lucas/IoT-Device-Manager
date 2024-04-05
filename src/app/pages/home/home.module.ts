43ca4bcea1971f251cab1e18e66d2f3cd4d4abd9
Flávio Lucas <flaviolucas389@gmail.com>
2024-04-05 15:54:58 -0300

    fix(header): added header in home page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from "./home-routing.module";
+import {HeaderModule} from "../../components/header/header.module";
 
 @NgModule({
   declarations: [
     HomeComponent,
   ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-  ]
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+        HeaderModule,
+    ]
 })
 export class HomeModule { }
