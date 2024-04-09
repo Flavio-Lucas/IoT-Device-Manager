c6ebaed701c09553c2851d0e9cbc47243e627ebd
Flávio Lucas <flaviolucas389@gmail.com>
2024-04-09 16:12:47 -0300

    fix(login): fixed login layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {StorageService} from "../../services/storage/storage.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -10,11 +11,13 @@ export class HeaderComponent {
 
   constructor(
     public storageService: StorageService,
+    public router: Router,
   ) {
   }
 
   public async logout(): Promise<void> {
-    await this.storageService.clear()
+    await this.storageService.clear();
+    await this.router.navigateByUrl('/login');
   }
 
 }
