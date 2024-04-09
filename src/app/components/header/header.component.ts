cf2e0238922fe7fdf209f125a34358787ef15294
Flávio Lucas <flaviolucas389@gmail.com>
2024-04-09 16:07:01 -0300

    feat(logout): added logout function

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {StorageService} from "../../services/storage/storage.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(
+    public storageService: StorageService,
+  ) {
+  }
 
-  ngOnInit(): void {
+  public async logout(): Promise<void> {
+    await this.storageService.clear()
   }
 
 }
